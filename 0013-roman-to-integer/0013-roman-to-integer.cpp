3e40aeb6f776acc99a3a74b24d5340dc6aed8820
Sunny Arya <135229069+sunnyarya0@users.noreply.github.com>
2026-06-30 07:54:49 +0530

    Time: 5 ms (60%), Space: 10 MB (94.21%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,22 +1,40 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-        unordered_map<char, int> value = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500},
-            {'M', 1000}
-        };
-
-        int result = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            if (i + 1 < s.size() && value[s[i]] < value[s[i + 1]]) {
-                result -= value[s[i]];
-            } else {
-                result += value[s[i]];
-            }
+    int value(char ch){
+        int I=1,V=5,X=10,L=50,C=100,D=500,M=1000;
+        if(ch=='I'){
+            return 1;
         }
+        else if(ch=='V'){
+            return 5;
+        }
+        else if(ch=='X'){
+            return 10;
+        }
+        else if(ch=='L'){
+            return 50;
+        }
+        else if(ch=='C'){
+            return 100;
+        }
+        else if(ch=='D'){
+            return 500;
+        }
+        else{
+            return 1000;
+        }
+    }
 
-        return result;
+    int romanToInt(string s) {
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(value(s[i+1])>value(s[i]) && i<s.size()-1){
+                ans -= value(s[i]);
+            }
+            else{
+                ans+= value(s[i]);
+            }
+        }   
+        return ans;     
     }
 };
