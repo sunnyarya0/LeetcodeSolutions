81854bc329d5bbec3402dfef8ed275eb707faef5
Sunny Arya <135229069+sunnyarya0@users.noreply.github.com>
2026-06-30 07:55:18 +0530

    Time: 2 ms (78.23%), Space: 13.4 MB (18.07%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-    int value(char ch){
-        int I=1,V=5,X=10,L=50,C=100,D=500,M=1000;
-        if(ch=='I'){
-            return 1;
-        }
-        else if(ch=='V'){
-            return 5;
-        }
-        else if(ch=='X'){
-            return 10;
-        }
-        else if(ch=='L'){
-            return 50;
-        }
-        else if(ch=='C'){
-            return 100;
-        }
-        else if(ch=='D'){
-            return 500;
-        }
-        else{
-            return 1000;
-        }
-    }
-
     int romanToInt(string s) {
-        int ans=0;
-        for(int i=0;i<s.size();i++){
-            if(value(s[i+1])>value(s[i]) && i<s.size()-1){
-                ans -= value(s[i]);
+        unordered_map<char, int> value = {
+            {'I', 1}, {'V', 5}, {'X', 10},
+            {'L', 50}, {'C', 100}, {'D', 500},
+            {'M', 1000}
+        };
+
+        int result = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && value[s[i]] < value[s[i + 1]]) {
+                result -= value[s[i]];
+            } else {
+                result += value[s[i]];
             }
-            else{
-                ans+= value(s[i]);
-            }
-        }   
-        return ans;     
+        }
+
+        return result;
     }
 };
